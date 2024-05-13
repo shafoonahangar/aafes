a50036aa83317446385b98ea601741ca250cc398
shafoonahangar <shafoon.ahangar@icreon.com>
2024-05-13 20:51:22 +0530

    Update pre_loader.js

--- pre_loader.js
+++ pre_loader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://cdn.jsdelivr.net/gh/shafoonahanger/MyEcpExternalResource/snake_loader.svg';
+svgImage.src = 'https://cdn.jsdelivr.net/gh/shafoonahangar/aafes/star.png';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
